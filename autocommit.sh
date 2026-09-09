5385acf0bdc0be0dcec4a22893d5992ee1f55077
maddy-017 <bangiabdulmoid@gmail.com>
2026-09-09 17:04:20 +0530

    Auto-commit: 2026-09-09 17:04:20

--- autocommit.sh
+++ autocommit.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
-
-if pgrep -f "fswatch -o /Users/amueedbangi/COLLAGE" > /dev/null; then
-  exit 0
-fi
-
 cd /Users/amueedbangi/COLLAGE
-/opt/homebrew/bin/fswatch -o . | while read change; do
+
+if [ -n "$(git status --porcelain)" ]; then
   git add -A
   git commit -m "Auto-commit: $(date '+%Y-%m-%d %H:%M:%S')"
   git push origin main
-done
+fi
